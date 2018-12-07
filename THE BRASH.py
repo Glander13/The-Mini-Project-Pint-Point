1b50659256e6fffcc16f2dd17d82c6866b0f1a32
Glander <ivanchebykin4@gmail.com>
2018-12-07 19:19:47 +0400

    Создан первый набросок карандаша для paint'а и создан пустой файл MAIN

---
+++ THE BRASH.py
@@ -0,0 +1,144 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        top = 400
+        left = 400
+        width = 800
+        height = 600
+
+        self.setWindowTitle("Paint Application")
+        self.setGeometry(top, left, width, height)
+
+        self.image = QImage(self.size(), QImage.Format_RGB32)
+        self.image.fill(Qt.white)
+
+        self.drawing = False
+        self.brushSize = 2
+        self.brushColor = Qt.black
+
+        self.lastPoint = QPoint()
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu("File")
+        brashMenu = mainMenu.addMenu("Brush Size")
+        brashColor = mainMenu.addMenu("Brush Color")
+
+        saveAction = QAction("Save", self)
+        saveAction.setShortcut("Ctrl+S")
+        fileMenu.addAction(saveAction)
+        saveAction.triggered.connect(self.save)
+
+        clearAction = QAction("Clear", self)
+        clearAction.setShortcut("Ctrl+C")
+        fileMenu.addAction(clearAction)
+        clearAction.triggered.connect(self.clear)
+
+        thereepxAction = QAction("3px", self)
+        thereepxAction.setShortcut("Ctrl+T")
+        brashMenu.addAction(thereepxAction)
+        thereepxAction.triggered.connect(self.threePx)
+
+        fivepxAction = QAction("5px", self)
+        fivepxAction.setShortcut("Ctrl+F")
+        brashMenu.addAction(fivepxAction)
+        fivepxAction.triggered.connect(self.fivePx)
+
+        sevenpxAction = QAction("7px", self)
+        sevenpxAction.setShortcut("Ctrl+S")
+        brashMenu.addAction(sevenpxAction)
+        sevenpxAction.triggered.connect(self.sevenPx)
+
+        ninepxAction = QAction("9px", self)
+        ninepxAction.setShortcut("Ctrl+N")
+        brashMenu.addAction(ninepxAction)
+        ninepxAction.triggered.connect(self.ninePx)
+
+        blackAction = QAction("Black", self)
+        blackAction.setShortcut("Ctrl+B")
+        brashColor.addAction(blackAction)
+        blackAction.triggered.connect(self.blackColor)
+
+        redAction = QAction("Red", self)
+        redAction.setShortcut("Ctrl+W")
+        brashColor.addAction(redAction)
+        redAction.triggered.connect(self.redColor)
+
+        greenAction = QAction("Green", self)
+        greenAction.setShortcut("Ctrl+G")
+        brashColor.addAction(greenAction)
+        greenAction.triggered.connect(self.greenColor)
+
+        yellowAction = QAction("Yellow", self)
+        yellowAction.setShortcut("Ctrl+G")
+        brashColor.addAction(yellowAction)
+        yellowAction.triggered.connect(self.yellowColor)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.drawing = True
+            self.lastPoint = event.pos()
+
+    def mouseMoveEvent(self, event):
+        if (event.buttons() & Qt.LeftButton) & self.drawing:
+            painter = QPainter(self.image)
+            painter.setPen(QPen(self.brushColor, self.brushSize, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+            painter.drawLine(self.lastPoint, event.pos())
+            self.lastPoint = event.pos()
+            self.update()
+
+    def mouseReleaseEvent(self, event):
+        if event.button == Qt.LeftButton:
+            self.drawing = False
+
+    def paintEvent(self, event):
+        canvasPainter = QPainter(self)
+        canvasPainter.drawImage(self.rect(), self.image, self.image.rect())
+
+    def save(self):
+        filePath, _ = QFileDialog.getSaveFileName(self, "Save Image", "",
+                                                  "PNG(*.png);;JPEG(*.jpg *.jpeg);; ALL Files(*.*)")
+        if filePath == "":
+            return
+        self.image.save(filePath)
+
+    def clear(self):
+        self.image.fill(Qt.white)
+        self.update()
+
+    def threePx(self):
+        self.brushSize = 3
+
+    def fivePx(self):
+        self.brushSize = 5
+
+    def sevenPx(self):
+        self.brushSize = 7
+
+    def ninePx(self):
+        self.brushSize = 9
+
+    def blackColor(self):
+        self.brushColor = Qt.black
+
+    def redColor(self):
+        self.brushColor = Qt.red
+
+    def greenColor(self):
+        self.brushColor = Qt.green
+
+    def yellowColor(self):
+        self.brushColor = Qt.yellow
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    app.exec()

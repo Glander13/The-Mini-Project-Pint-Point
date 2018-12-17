a84b1f3b470c2d1cb4e9f7971f4ead316b68ce43
Glander13 <ivanchebykin4@gmail.com>
2018-12-17 23:35:41 +0400

    маленькие поправки

--- MAIN.py
+++ MAIN.py
@@ -1,10 +1,9 @@
-import random
-from os import system
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import random
+from os import system
 
 
 # Скрипт окна
